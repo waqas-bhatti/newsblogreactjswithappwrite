8b12f17da8e481ba62c2ac0e750bb7afe199bd62
waqas-bhatti <mwaqas8795@gmail.com>
2024-12-10 20:40:30 +0500

    Add existing project files to Git

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,17 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor"; // Puts all node_modules in vendor.js
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Warn if chunk size > 1000 KB (1 MB)
+    sourcemap: true, // Enable source maps for easier debugging
+  },
 });
